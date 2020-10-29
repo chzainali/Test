61e5ece6845045495aa2b04778e9fde7fa66a9aa
CH ZAIN ALI <zainalianwar12@gmail.com>
2020-10-30 00:07:02 +0500

    Version 2 is Posted

---
+++ app/src/main/java/app/techland/newproject/MainActivity.java
@@ -0,0 +1,16 @@
+package app.techland.newproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Github Code Done", Toast.LENGTH_SHORT).show();
+    }
+}
